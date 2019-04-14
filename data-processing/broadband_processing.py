9e3b45f0c6059374b6403622c1fde50b44348936
Thomas Ernste <tjernste@gmail.com>
2019-04-14 13:50:14 -0700

    Improving efficiency of script

--- data-processing/broadband_processing.py
+++ data-processing/broadband_processing.py
@@ -1,4 +1,4 @@
-# This script processes a dataset available in an S3 bucket conataining         # information about broadband availability across the United States.            # Specifically, the data shows available broadband speeds,the broadband         # infrastrucure technologies, and the provider names for broadband systems      # across the more than 11 million US census blocks in the United States.
+# This script processes a dataset available in an S3 bucket conataining information about broadband availability across the United States.            # Specifically, the data shows available broadband speeds,the broadband         # infrastrucure technologies, and the provider names for broadband systems      # across the more than 11 million US census blocks in the United States.
 
 
 
@@ -27,7 +27,7 @@ def extract_transform_load_broadband(input_data_txt, output_data_txt):
 
 #    rdd_BROADBAND = sc.textFile(input_data_txt)
 
-    # This file is a comma separated file and has some commas embedded within       # string objects within some of the columns. The file is first read in to       # an RDD and this function is used in a map transformation (below) to           # remove those commas.
+    # This file is a comma separated file and has some commas embedded within string objects within some of the columns. The file is first read in to       # an RDD and this function is used in a map transformation (below) to           # remove those commas.
 
 #    def remove_commas(row):
 
@@ -35,7 +35,7 @@ def extract_transform_load_broadband(input_data_txt, output_data_txt):
 
 #        return row
 
-    # The first 11 numbers of a census block number match the 11 numbers in a       # census tract number. While the census block numbers in this file are 15       # digits long, the other datasets to be joined with this file for the           # database (containing housing data and zip codes) only have the 11 digit       # census tract data. Therefore, this function is used in a map                  # transformation (below) to keep only the first 11 numbers of the census        # block numbers. This column will be used to join this file by other files      # containing census tract numbers.
+    # The first 11 numbers of a census block number match the 11 numbers in a census tract number. While the census block numbers in this file are 15       # digits long, the other datasets to be joined with this file for the           # database (containing housing data and zip codes) only have the 11 digit       # census tract data. Therefore, this function is used in a map                  # transformation (below) to keep only the first 11 numbers of the census        # block numbers. This column will be used to join this file by other files      # containing census tract numbers.
 
 #    def reduce_census_block_code(row):
 
@@ -43,14 +43,14 @@ def extract_transform_load_broadband(input_data_txt, output_data_txt):
 
 #        return row
 
-    # This block removes the header, which is necessary for implementing the        # code below.
+    # This block removes the header, which is necessary for implementing the code below.
 
 #    header = rdd_BROADBAND.first()
 
 #    rdd_BROADBAND = rdd_BROADBAND.filter(lambda line: line != header)
 
 
-    # This block of code removes ascii unicode information, removes commas          # embedded in within strings in some of the columns, splits the RDD object      # on the commas, and reduces the length of the census blocks from 15 to 11      # digits.
+    # This block of code removes ascii unicode information, removes commas embedded in within strings in some of the columns, splits the RDD object      # on the commas, and reduces the length of the census blocks from 15 to 11      # digits.
 
  #   rdd_transformed_BROADBAND = rdd_BROADBAND\
  #                                   .map(lambda x: x.encode('ascii', 'ignore'))\
@@ -104,7 +104,7 @@ def extract_transform_load_broadband(input_data_txt, output_data_txt):
 #                                .withColumnRenamed("_c16", "max_cir_upstr_speed")
 
 
-    # This code selects and saves just seven of the 16 columns from the original    # file that have some clear potential value for the database.
+    # This code selects and saves just seven of the 16 columns from the original file that have some clear potential value for the database.
 
 
 #    df_transformed_BROADBAND = df_transformed_BROADBAND\
